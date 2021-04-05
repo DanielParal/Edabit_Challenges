c27e1f669e9ccd70a31b569eca32a26344cbf606
Daniel Paral <dparal@Identigen.com>
2021-04-05 09:36:19 +0200

    Added challenge "Track the robot"

---
+++ DefaultProject/DefaultProject/TrackTheRobot/TrackTheRobot.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefaultProject.TrackTheRobot
+{
+    public enum EDirection
+    {
+        // east
+        E = 0,
+        // south
+        S = 1,
+        // west
+        W = 2,
+        // north
+        N = 3
+    }
+
+    public class TrackTheRobot
+    {
+        public static int[] TrackRobot(string steps)
+        {
+            int[] currentDirection = new int[] { 0, 0 };
+            EDirection direction = EDirection.E;
+
+            foreach (char step in steps)
+            {
+                // if step not ><. -> skip
+                // if step . -> move
+                // if step >< -> change direction
+                if (step == '.')
+                    currentDirection = Move(currentDirection, direction);
+
+                if (step == '>' || step == '<')
+                    direction = ChangeDirection(direction, step);
+            }
+
+            return currentDirection;
+        }
+
+        public static EDirection ChangeDirection(EDirection currentDirection, char sign)
+        {
+            int direction = (int)currentDirection;
+            if (sign == '>')
+                direction += 1;
+
+            if (sign == '<')
+                direction += -1;
+
+            if (direction < 0)
+                return EDirection.N;
+
+            if (direction > 3)
+                return EDirection.E;
+
+            return (EDirection)direction;
+        }
+
+        public static int[] Move(int[] currentPosition, EDirection direction)
+        {
+            switch (direction)
+            {
+                case EDirection.E:
+                    return new int[2] { currentPosition[0] + 1, currentPosition[1] };
+                case EDirection.S:
+                    return new int[2] { currentPosition[0], currentPosition[1] - 1 };
+                case EDirection.W:
+                    return new int[2] { currentPosition[0] - 1, currentPosition[1] };
+                default:
+                    // north
+                    return new int[2] { currentPosition[0], currentPosition[1] + 1 };
+            }
+        }
+    }
+}
---
+++ Tests/DefaultProject.Tests/TrackTheRobotTest/TrackTheRobotTest.cs
@@ -0,0 +1,50 @@
+﻿using DefaultProject.TrackTheRobot;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DefaultProject.Tests.TrackTheRobotTest
+{
+    public class TrackTheRobotTest
+    {
+        [Theory]
+        [InlineData("..<.<.", new int[] { 1, 1 })]
+        [InlineData("....................................................................................................", new int[] { 100, 0 })]
+        [InlineData("<>>>><><<<><>>>><><<<><>>><>", new int[] { 0, 0 })]
+        [InlineData(".<..<...<....<.....<......", new int[] { 3, 4 })]
+        [InlineData(">>..", new int[] { -2, 0 })]
+        [InlineData("..<<..>>..<<..>>..", new int[] { 2, 0 })]
+        public void TrackRobot(string steps, int[] result)
+        {
+            int[] actualResult = TrackTheRobot.TrackTheRobot.TrackRobot(steps);
+            Assert.Equal(result, actualResult);
+        }
+
+        [Theory]
+        [InlineData(EDirection.E, '>', EDirection.S)]
+        [InlineData(EDirection.S, '<', EDirection.E)]
+        [InlineData(EDirection.E, '<', EDirection.N)]
+        [InlineData(EDirection.N, '>', EDirection.E)]
+        public void ChangeDirection_ShouldClockwiseIfBiggerSign_ShouldAnticlockwiseIfSmallerSign(EDirection direction, char sign, EDirection result)
+        {
+            EDirection actualDirection = TrackTheRobot.TrackTheRobot.ChangeDirection(direction, sign);
+            Assert.Equal(result, actualDirection);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 0, 0 }, EDirection.E, new int[] { 1, 0 })]
+        [InlineData(new int[] { 0, 0 }, EDirection.W, new int[] { -1, 0 })]
+        [InlineData(new int[] { 0, 0 }, EDirection.N, new int[] { 0, 1 })]
+        [InlineData(new int[] { 0, 0 }, EDirection.S, new int[] { 0, -1 })]
+        [InlineData(new int[] { 1, 1 }, EDirection.E, new int[] { 2, 1 })]
+        [InlineData(new int[] { 1, 1 }, EDirection.W, new int[] { 0, 1 })]
+        [InlineData(new int[] { 1, 1 }, EDirection.N, new int[] { 1, 2 })]
+        [InlineData(new int[] { 1, 1 }, EDirection.S, new int[] { 1, 0 })]
+        public void Move(int[] currentPosition, EDirection direction, int[] result)
+        {
+            int[] actualPosition = TrackTheRobot.TrackTheRobot.Move(currentPosition, direction);
+            Assert.Equal(result, actualPosition);
+        }
+    }
+}
